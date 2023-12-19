e847feedf733bd110798f3f8ca0bdf7f1b116553
takahashi <ryo.takahashi.sys22@morijyobi.ac.jp>
2023-12-19 14:34:23 +0900

    太郎と花子を表示させた。カーソルキーで移動させた close #4, #5, #6

--- scripts/main_scene.js
+++ scripts/main_scene.js
@@ -20,15 +20,39 @@ class MainScene extends Phaser.Scene {
         // 単体画像をシーンに追加(X座標,Y座標,画像名)
        this.add.image(400, 300, 'background');
        const taro = this.physics.add.sprite(50, 50, 'taro')
-       const hanako = this.physics.add.sprite(500, 300, 'hanako')
+       const hanako = this.physics.add.sprite(750, 400, 'hanako')
        this.taro = taro
        this.hanako = hanako
+
        }
 
      // 毎フレーム実行される繰り返し処理
-    update(time, delta) {
-
+        update() {
+            // キーボードの情報を取得
+            let cursors = this.input.keyboard.createCursorKeys();
+            if(cursors.up.isDown){
+                console.log("Up!!");
+                this.taro.setVelocityY(-200);// 上方向の速度を設定
+                this.hanako.setVelocityY(200);
+            } else if(cursors.down.isDown){
+                console.log("down!!");
+                this.taro.setVelocityY(200);// 下方向の速度を設定
+                this.hanako.setVelocityY(-200);
+            }else if(cursors.left.isDown){
+                console.log("Left");
+                this.taro.setVelocityX(-200);// 左方向の速度を設定
+                this.hanako.setVelocityX(200);
+            }else if(cursors.right.isDown){
+                console.log("Right!!");
+                this.taro.setVelocityX(200);// 右方向の速度を設定
+                this.hanako.setVelocityX(-200);
+            }else{
+                this.taro.setVelocityX(0);// 横方向の速度を0
+                this.taro.setVelocityY(0);// 縦方向の速度を0
+                this.hanako.setVelocityX(0);// 横方向の速度を0
+                this.hanako.setVelocityY(0);// 縦方向の速度を0
+            }
     }
-
 }
+
 
