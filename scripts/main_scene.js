fa1a2bea82e5a440dbd08971f49c4cac03127d04
takahashi <ryo.takahashi.sys22@morijyobi.ac.jp>
2023-12-19 14:16:56 +0900

    hanakoとtaroを配置した

--- scripts/main_scene.js
+++ scripts/main_scene.js
@@ -1,26 +1,34 @@
 // シーンクラス
 // 他のJSファイルから呼び出された場合はシーンを返す
 class MainScene extends Phaser.Scene {
+    
     // コンストラクタ
     constructor() {
         // 継承した「Phaser.Scene」クラスのコンストラクタの呼び出し
         super('MainScene');
     }
 
-
     // シーンの事前読み込み処理
     preload() {
-        this.load.image('sky', 'assets/bg_dote_yoru.jpg');
+        // 画像の読み込み(使用する時の名前, パス)
+       this.load.image('background', 'assets/back.png');
+       this.load.image('taro', 'assets/taro.png');
+       this.load.image('hanako', 'assets/hanako.png');
+   }
+   // シーン初期化処理
+   create() {
+        // 単体画像をシーンに追加(X座標,Y座標,画像名)
+       this.add.image(400, 300, 'background');
+       const taro = this.physics.add.sprite(50, 50, 'taro')
+       const hanako = this.physics.add.sprite(500, 300, 'hanako')
+       this.taro = taro
+       this.hanako = hanako
+       }
+
+     // 毎フレーム実行される繰り返し処理
+    update(time, delta) {
+
     }
 
-
-    // シーン初期化処理
-
-    create() {
-        this.add.image(400, 300, 'sky');
-    }
-
-    // 毎フレーム実行される繰り返し処理
-    update() {
-    }
 }
+
