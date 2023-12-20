23bec47dcbea0055b31669b92db40cb998685dc7
takahashi <ryo.takahashi.sys22@morijyobi.ac.jp>
2023-12-20 13:52:59 +0900

    git commit -m "APPLEとORANGEを表示させた。ランダムに表示" close #8, #9

--- scripts/main_scene.js
+++ scripts/main_scene.js
@@ -1,19 +1,19 @@
 // シーンクラス
 // 他のJSファイルから呼び出された場合はシーンを返す
 class MainScene extends Phaser.Scene {
-    
     // コンストラクタ
     constructor() {
         // 継承した「Phaser.Scene」クラスのコンストラクタの呼び出し
         super('MainScene');
     }
-
     // シーンの事前読み込み処理
     preload() {
         // 画像の読み込み(使用する時の名前, パス)
        this.load.image('background', 'assets/back.png');
        this.load.image('taro', 'assets/taro.png');
        this.load.image('hanako', 'assets/hanako.png');
+       this.load.image('APPLE', 'assets/APPLE.png');
+       this.load.image('ORANGE', 'assets/ORANGE.png');
    }
    // シーン初期化処理
    create() {
@@ -23,9 +23,18 @@ class MainScene extends Phaser.Scene {
        const hanako = this.physics.add.sprite(750, 400, 'hanako')
        this.taro = taro
        this.hanako = hanako
-
+       for (let i = 0; i < 6; i++) {
+        let randx = Phaser.Math.Between(25, 775);
+        let randy = Phaser.Math.Between(25, 425);
+        this.add.image(randx, randy, 'APPLE');
+    }
+    // ６つのORANGEをランダムに配置
+    for (let i = 0; i < 6; i++) {
+        let randxx = Phaser.Math.Between(25, 775);
+        let randyy = Phaser.Math.Between(25, 425);
+        this.add.image(randxx, randyy, 'ORANGE');
+    }
        }
-
      // 毎フレーム実行される繰り返し処理
         update() {
             // キーボードの情報を取得
@@ -52,8 +61,5 @@ class MainScene extends Phaser.Scene {
                 this.hanako.setVelocityX(0);// 横方向の速度を0
                 this.hanako.setVelocityY(0);// 縦方向の速度を0
             }
-            
     }
 }
-
-
